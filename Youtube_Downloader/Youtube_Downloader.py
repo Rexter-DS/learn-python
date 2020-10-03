b78d565d6586c856499cf7f6e3872944c45d2dc6
Rexter Delos Santos <rexterds@hawaii.edu>
2020-10-02 23:04:54 -1000

    Added a default download path to the Downloads folder

--- Youtube_Downloader/Youtube_Downloader.py
+++ Youtube_Downloader/Youtube_Downloader.py
@@ -7,12 +7,17 @@ def main():
 
     youtube_link = input()
 
-    download_youtube_audio(youtube_link)
+    download_youtube_audio(youtube_link, default_path())
 
 # Downloads just the audio from a youtube video
-def download_youtube_audio(youtube_link):
-    yt = YouTube(youtube_link) 
+def download_youtube_audio(yt_link, path):
+    yt = YouTube(yt_link) 
     audio_stream = yt.streams.get_audio_only()
-    audio_stream.download()
+    audio_stream.download(path)
+
+#initiates a default download path to the user's download folder
+def default_path():
+    download_path = Path().home().joinpath("Downloads")
+    return download_path
 
 main()
