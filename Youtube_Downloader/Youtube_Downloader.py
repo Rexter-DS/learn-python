2101be418943e18a0e096064ac010a9fca358156
Rexter Delos Santos <rexterds@hawaii.edu>
2020-10-06 10:32:33 -1000

    Setup option for downloading video

--- Youtube_Downloader/Youtube_Downloader.py
+++ Youtube_Downloader/Youtube_Downloader.py
@@ -2,12 +2,28 @@ from pytube import YouTube
 from pathlib import Path
 
 def main():
-    print("Welcome to Youtube to MP4")
-    print("To get started, please enter a youtube link")
-
+    print("Welcome to Youtube to MP4 Converter")
+    print("To get started please enter your youtube link")
     youtube_link = input()
 
-    download_youtube_audio(youtube_link, default_path())
+    print("Type 1 or 2 to specify whether you would like to download the full video (with sound) or just the audio")
+    
+    while True:
+        try:
+            choice = int(input("Type 1 for video or 2 for audio: "))
+        except ValueError:
+            print("Invalid option")
+            continue
+        
+        if choice in (1, 2):
+            break
+        else:
+            print("Invalid option")
+
+    if choice == 1:
+        print("Something")
+    elif choice == 2:
+        download_youtube_audio(youtube_link, default_path())
 
 # Downloads just the audio from a youtube video
 def download_youtube_audio(yt_link, path):
